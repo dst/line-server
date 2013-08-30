a63c98f1ddd395255f64c66e39294dba7176ff03
Dariusz Stefanski <d.a.stefanski@gmail.com>
2013-08-31 01:09:02 +0200

    Creating example app using Maven

---
+++ line-server/src/main/java/com/stefanski/lineserver/App.java
@@ -0,0 +1,13 @@
+package com.stefanski.lineserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
