56dd76d3f8e3b493916235fb3a8f0dc638f7ac18
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-06-10 00:23:31 +0200

    Don't run unit tests when testing performance

--- test/test-performance.sh
+++ test/test-performance.sh
@@ -3,7 +3,7 @@
 # Date: 05.09.2013
 # Author: Dariusz Stefanski
 #
-# It runs unit and _performance_ tests of Liner application.
+# It runs performance tests of Liner application.
 
 MAX_LINE_NR="400000000"
 FILE="file4x10^8.txt"
@@ -18,5 +18,3 @@ export MAVEN_OPTS="-Xms$ms -Xmx$mx"
 export PerformanceTesting="true"
 export TestFile=`pwd`/$FILE
 export MaxLineNr=$MAX_LINE_NR
-
-mvn test  -f ../liner/pom.xml
