320a49c3cbfa66a36035d91f895e2bb349de18a1
Dariusz Stefanski <d.a.stefanski@gmail.com>
2015-10-17 23:37:56 +0200

    Optional usage

--- src/main/java/com/stefanski/liner/server/resp/LineResponse.java
+++ src/main/java/com/stefanski/liner/server/resp/LineResponse.java
@@ -2,9 +2,13 @@ package com.stefanski.liner.server.resp;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 
 /**
  * A response for a line command.
@@ -21,17 +25,17 @@ public class LineResponse implements Response {
     private static final String ERROR = "ERROR" + RESP_END_OF_LINE;
 
     private final String status;
-    private final String line;
+    private final Optional<String> line;
 
     public static LineResponse createOkResp(String line) {
-        return new LineResponse(OK, line);
+        return new LineResponse(OK, of(line));
     }
 
     public static LineResponse createErrResp() {
-        return new LineResponse(ERROR, null);
+        return new LineResponse(ERROR, empty());
     }
 
-    public LineResponse(String status, String line) {
+    private LineResponse(String status, Optional<String> line) {
         this.status = status;
         this.line = line;
     }
@@ -39,8 +43,8 @@ public class LineResponse implements Response {
     @Override
     public void write(PrintWriter writer) throws IOException {
         writer.print(status);
-        if (line != null) {
-            writer.println(line);
+        if (line.isPresent()) {
+            writer.println(line.get());
         }
     }
 }
